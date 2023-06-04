998da2e7f3c4d0cc3749b2624f47db776167e356
Moresco <rafaelmorescovieira@gmail.com>
2023-06-04 15:05:52 -0300

    basic FA reader

--- reader.py
+++ reader.py
@@ -1,2 +1,22 @@
+
 class Reader():
-    pass
+    
+
+    def readFile(self, fileName):
+        f = open(fileName,"r")
+        content = f.readlines()
+        f.close()
+        return content
+
+    def readAF(self, fileName: str):
+        content = self.readFile(fileName)
+        matriz = []
+        for line in content:
+            line = line.strip('\n')
+            matriz.append(line.split(sep=','))
+        return matriz
+
+
+    def readGr():
+        pass
+
