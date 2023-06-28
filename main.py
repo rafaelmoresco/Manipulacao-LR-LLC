e400eb39ff460b720d110457d2f3f2e5858e203d
Moresco <rafaelmorescovieira@gmail.com>
2023-06-28 00:59:11 -0300

    add RG operations to main

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from reader import Reader
 from FiniteAutomata import FiniteAutomata
 from FiniteAutomataUtils import FiniteAutomataUtils
+from RegularGrammar import GR
 from RegEx import RegexTree
 
 reader = Reader()
 algoritm = FiniteAutomataUtils()
+regularGrammar = GR()
 
 while True:
     print("Digite a operação: ",end='')
     x = input()
-    if x == "determinizar":
+    if x == "determinizarAF":
         print("Digite o arquivo de origem: ",end='')
         y = input()
         inputAutomata: FiniteAutomata = reader.readAF(y)
         inputAutomata.determinize()
-    elif x == "minimizar":
+    elif x == "minimizarAF":
         print("Digite o arquivo de origem: ",end='')
         y = input()
         inputAutomata: FiniteAutomata = reader.readAF(y)
         inputAutomata.minimize()
-    elif x == "uniao":
+    elif x == "uniaoAF":
         print("Digite o primeiro aquivo de origem: ",end='')
         y = input()
         print("Digite o segundo aquivo de origem: ",end='')
@@ -27,7 +29,7 @@ while True:
         inputAutomata: FiniteAutomata = reader.readAF(y)
         inputAutomata2: FiniteAutomata = reader.readAF(z)
         new = algoritm.dfaUnion(inputAutomata, inputAutomata2)
-    elif x == "intersecao":
+    elif x == "intersecaoAF":
         print("Digite o primeiro aquivo de origem: ",end='')
         y = input()
         print("Digite o segundo aquivo de origem: ",end='')
@@ -50,6 +52,17 @@ while True:
             print("Palavra reconhecida!")
         else:
             print("Palavra não reconhecida!")
+    elif x == "GRparaAF":
+        print("Digite o arquivo de origem: ",end='')
+        y = input()
+        gr = reader.readGr(y)
+        new = regularGrammar.GRparaAF(gr)
+        new.outputToFile("RGtoFA")
+    elif x == "AFparaGR":
+        print("Digite o arquivo de origem: ",end='')
+        y = input()
+        inputAutomata: FiniteAutomata = reader.readAF(y)
+        regularGrammar.AFparaGR(inputAutomata)
     elif x == "sair":
         exit()
         break
